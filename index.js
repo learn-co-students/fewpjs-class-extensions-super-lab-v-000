aa14e1bc1a62031e0511d21fd397940e90167c34
Meg Gutshall <meghan.gutshall@gmail.com>
2020-04-04 10:13:19 -0400

    Done.

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ class Deciduous extends Tree {
   }
 
   static definition() {
-    return (
-      super.definition() + " Deciduous trees shed their leaves annually."
-    );
+    return `${super.definition()} Deciduous trees shed their leaves annually.`;
   }
 }
 
@@ -28,8 +26,6 @@ class Evergreen extends Tree {
   }
 
   static definition() {
-    return (
-      super.definition() + " Evergreens keep their leaves all year round."
-    );
+    return `${super.definition()} Evergreens keep their leaves all year round.`;
   }
 }
